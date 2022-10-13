ae3ce8375e85317ee47c2a3945c4dae4a0a61a71
Yvonnou Théo <tyvonnou@gmail.com>
2022-10-13 18:52:11 +0200

    [scenes] - clean sprite annimation

--- scenes/personnages/escargotVert/Personnage.gd
+++ scenes/personnages/escargotVert/Personnage.gd
@@ -4,23 +4,37 @@ onready var _animated_sprite = $EscargotVert
 var speed = 170
 var velocity = Vector2()
 
-func get_input():
+func get_input() -> void:
 	velocity = Vector2()
-		
+	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
+		_animated_sprite.flip_h = false
+		_animated_sprite.flip_v = false
 		_animated_sprite.play("right")
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-		_animated_sprite.play("left")
+		_animated_sprite.flip_h = true
+		_animated_sprite.flip_v = false
+		_animated_sprite.play("right")
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
-		_animated_sprite.play("down")
+		_animated_sprite.flip_h = false
+		_animated_sprite.flip_v = true
+		_animated_sprite.play("up")
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+		_animated_sprite.flip_h = false
+		_animated_sprite.flip_v = false
 		_animated_sprite.play("up")
-	
+	 
+	if (Input.is_action_just_released("ui_up") || Input.is_action_just_released("ui_down") ||
+		Input.is_action_just_released("ui_left") || Input.is_action_just_released("ui_right")):
+			_animated_sprite.stop()
+			
 	velocity = velocity.normalized() * speed
+	
+	
 
 func _physics_process(delta):
 	get_input()
