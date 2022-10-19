8bcab21cedcea842efeb19c7e1fe030ac4e86db4
Yvonnou Théo <tyvonnou@gmail.com>
2022-10-19 23:12:29 +0200

    work in progress

--- scenes/personnages/hero/HeroBody.gd
+++ scenes/personnages/hero/HeroBody.gd
@@ -13,21 +13,27 @@ func _process(_delta: float) -> void:
 	)
 
 	# Play movements and update look direction
-	if (input_vector.length() > 0.0 &&  !$SwordBody/StarSprite.playing):
+	if (input_vector.length() > 0.0):
 			personnage.look_direction = input_vector
-			$HeroSprite.flip_h = sign(personnage.look_direction.x) == -1.0
+			if $HeroSprite.get_animation() != "sword-up-load":
+				$HeroSprite.flip_h = sign(personnage.look_direction.x) == -1.0
+			# Basic movement 
 			if get_parent().SPRITE_MAP[input_vector].begins_with("mv-") && !Input.is_action_pressed("ui_accept"):
 				$HeroSprite.play(get_parent().SPRITE_MAP[input_vector])
 				$SwordBody.stopAnimation($SwordBody/SwordSprite)
+			# Load sword movement 
+			if get_parent().SPRITE_MAP[input_vector].begins_with("mv-") && !$HeroSprite.playing:
+				$HeroSprite.playing = true
 	
 	# Stop movement
 	if (Input.is_action_just_released("ui_right") || 
 	Input.is_action_just_released("ui_left") || 
 	Input.is_action_just_released("ui_down") || 
 	Input.is_action_just_released("ui_up")):
-		if $HeroSprite.get_animation().begins_with("mv-"):
-			$HeroSprite.stop()
-			$HeroSprite.frame = 0
+		match $HeroSprite.get_animation():
+			"mv-bottom-right", "mv-down", "mv-right", "mv-up", "mv-up-right", "sword-up-load":
+				$HeroSprite.stop()
+				$HeroSprite.frame = 0
 		
 	if (Input.is_action_just_pressed("ui_accept") && equippedSword):
 		get_tree().get_root().set_disable_input(true)
@@ -40,13 +46,11 @@ func _process(_delta: float) -> void:
 				$HeroSprite.play("sword-down")
 
 	if Input.is_action_just_released("ui_accept") && personnage.swordLoad:
-		match personnage.SPRITE_MAP[personnage.look_direction]:
-			"mv-right":
-				$HeroSprite.play("sword-right")
-			"mv-up", "mv-up-right":
+		match $HeroSprite.get_animation():
+			"sword-up-load":
 				$HeroSprite.flip_h = false 
 				$HeroSprite.play("sword-up-release")
-			"mv-down", "mv-bottom-right":
+			"sword-down-load":
 				$HeroSprite.play("sword-down-release")
 		
 func _on_Hero_animation_finished():
@@ -54,10 +58,17 @@ func _on_Hero_animation_finished():
 		match $SwordBody/SwordSprite.get_animation():
 			"sword-up":
 				$HeroSprite.play("sword-up-load") 
+				$HeroSprite.playing = false
 			"sword-down":
 				$HeroSprite.play("sword-down-load")    
 
 	else:
+		match $HeroSprite.get_animation():
+			"sword-up-release":
+				$HeroSprite.play("mv-up") 
+			"sword-down":
+				$HeroSprite.play("sword-down-load")  
+				
 		get_tree().get_root().set_disable_input(false)
 		$HeroSprite.stop()
 		$HeroSprite.frame = 0
